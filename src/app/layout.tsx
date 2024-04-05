cfb3d064f45e1d439043c5278aa520d575b2a948
PORAMART TSURIYARUNGSRI <pongthebigbear@gmail.com>
2024-04-05 11:57:32 +0700

    upddate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import BootstrapClient from "@/components/BootstrapClient";
 import "./globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import BootstrapClient from "@/components/BootstrapClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
-import styles from "./pagestyle.css";
-
+import "./pagestyle.css";
+import { Container, Row, Col } from "react-bootstrap";
 export default function Home() {
   return (
     <>
-    เป็นควยไรจ๊ะ
+      <Container>
+        <Row className="  ">
+          <Col className="topwidth d-flex align-items-center justify-content-center">
+          <h1 >
+            เป็นควยไรจ๊ะ
+            </h1>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
